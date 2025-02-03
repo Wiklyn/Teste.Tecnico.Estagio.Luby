d291e550f068494bc786c102159b54321c5be694
Wiklyn Craveiro <wiklynsc@gmail.com>
2025-02-03 05:28:01 -0300

    Adiciona o model referente a uma Venda, no desafio de POO

---
+++ Teste.Tecnico.Estagio.Luby/POO/Models/VendaModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Teste.Tecnico.Luby.POO.Models
+{
+    public class VendaModel
+    {
+        private static int _contadorId = 1;
+
+        public int Id { get; private set; }
+        public ProdutoModel Produto { get; set; }
+        public List<ItemVendaModel> ItensVendidos { get; set; } = [];
+        public decimal ValorTotal => ItensVendidos.Sum(item => item.ValorTotal);
+
+        public VendaModel()
+        {
+            Id = _contadorId++;
+        }
+    }
+}
