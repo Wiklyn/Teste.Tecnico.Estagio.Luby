2a98bf5ee0ae6f294479ccf71ea37b57b80fe162
Wiklyn Craveiro <wiklynsc@gmail.com>
2025-02-03 05:07:35 -0300

    Adicionar arquivos de projeto.

---
+++ Teste.Tecnico.Estagio.Luby/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Teste.Tecnico.Estagio.Luby
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
