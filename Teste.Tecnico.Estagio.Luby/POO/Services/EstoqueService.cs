a199fcabe12de12690f3b230069b64c29b252222
Wiklyn Craveiro <wiklynsc@gmail.com>
2025-02-03 05:32:50 -0300

    Adiciona o Service referente ao Estoque, no desafio de POO

---
+++ Teste.Tecnico.Estagio.Luby/POO/Services/EstoqueService.cs
@@ -0,0 +1,51 @@
+﻿using Teste.Tecnico.Luby.POO.Models;
+
+namespace Teste.Tecnico.Luby.POO.Services
+{
+    public class EstoqueService
+    {
+        public void ExibirEstoque(EstoqueModel estoque)
+        {
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.WriteLine("| ID  | Produto                     | Preço/u  | Quantidade |");
+            Console.WriteLine("-------------------------------------------------------------");
+
+            ProdutoModel.ReiniciarContadorId();
+
+            foreach (ProdutoModel produto in estoque.Produtos)
+            {
+                Console.WriteLine($"| {produto.Id,-3} | {produto.Nome,-27} | {produto.Valor,7:C} | {produto.Quantidade,10} |");
+            }
+
+            Console.WriteLine("-------------------------------------------------------------");
+        }
+
+        public char PerguntarSeDesejaVerOEstoqueAtualizado()
+        {
+            char comprarMais = ' ';
+
+            bool entradaValida = false;
+
+            while (!entradaValida)
+            {
+                Console.Write("\nDeseja ver o estoque atualizado? (S/N): ");
+                string? input = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(input) && input.Length == 1)
+                {
+                    comprarMais = char.ToUpper(input[0]);
+
+                    if (comprarMais == 'S' || comprarMais == 'N')
+                        entradaValida = true;
+                    else
+                        Console.WriteLine("Opção inválida! Por favor, digite apenas 'S' para ver o estoque atualizado ou 'N' para encerrar a lista de compras.");
+                }
+                else
+                    Console.WriteLine("Entrada inválida! Por favor, digite apenas um caractere 'S' ou 'N'.");
+            }
+
+            return comprarMais;
+        }
+    }
+}
